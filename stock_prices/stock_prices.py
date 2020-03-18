706b58ac8c647fd8b81adbdce5b6e731d6a9b3d4
Jessica Cooper <jessicagcooper@gmail.com>
2020-03-18 18:49:41 -0500

    First pass solution for stock_prices complete.

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -2,28 +2,26 @@
 import argparse
 
 # stock_prices = [1370, 350, 900, 7000, 5800, 3500]
-stock_prices = [10, 50, 20]
+stock_prices = [10, 50, 20, 70]
 
 def find_max_profit(prices):
-  
+
 #first stock is bought at a price in the list, it cannot be the last price
 #second stock is sold at a price in the list, it cannot be the first price
 #also the second stock must be after the first stock in the list
 #the list will remain in the order given
 #we need to find the maximum difference between 2 of the numbers while adhereing to the rules above
-#first starting with index of 1 subtract each following value from that at index of 0, keep the maximum number
+#first starting with index of 1 subtract each following value from that at index of 0
+#find all possible profits and then find maximum
+  profits = []
   for i in range(0, len(prices)-1):
-    current_buy = prices[i]
+    buy = i
     for j in range(i+1, len(prices)):
       current_sell = prices[j]
-      for k in range (i+2, len(prices)):
-        next_sell = prices[k]
-    current_profit = current_sell - current_buy
-    next_profit = next_sell - current_buy
-    if next_profit > current_profit:
-      return next_profit
-    else: 
-      return current_profit
+      current_buy = prices[buy]
+      profit = current_sell - current_buy
+      profits.append(profit)
+  return max(profits)
 
 print(find_max_profit(stock_prices))
 
